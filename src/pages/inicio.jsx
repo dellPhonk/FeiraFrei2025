5e4d3cabd84723890d5a966095afb2a0f8d1f672
Murillo <cmcalapica@gmail.com>
2025-08-22 08:58:36 -0300

    comitando02

--- src/pages/inicio.jsx
+++ src/pages/inicio.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import logofrei from '../assets/logofrei.png';
+import BotaoInscreva from '../assets/bt_01.png';
 import { Link } from "react-router";
 import './inicio.scss'
 import '../styles/global.scss'
@@ -14,6 +15,31 @@ export default function Inicio() {
             <Link to={"/"} className="Batao">Conheça o Site Do Frei</Link>
             </div>
             </nav>
+
+        <div className="tituloPrincipal"> 
+            <h1>5° FEIRA</h1>
+            <h2>DAS PROFISSÕES</h2>
+            <p>“Esforço que  transforma,<br /> e o orgulho permanece”</p>
         </div>
+
+        <div className="botaoInscreva">
+        <a href="" target="_blank" rel="noreferrer">
+          <img className="inscrevaIMG" src={BotaoInscreva} alt="Inscreva-se" />
+        </a>
+      </div>
+      <section class="event-info">
+    <div>
+      <h3>INICIO:</h3>
+      <p>09:00 DA MANHÃ</p>
+    </div>
+    <div class="date">
+      <span>📅 29/09</span>
+    </div>
+    <div>
+      <h3>TERMINO:</h3>
+      <p>14:00 DA TARDE</p>
+    </div>
+  </section>
+      </div>
     )
 }
