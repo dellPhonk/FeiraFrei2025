6208ec1c14bf5ebf4a29d9d0fe84f27e62ce2d74
Murillo <cmcalapica@gmail.com>
2025-08-22 07:48:48 -0300

    Comitando finalmente

--- src/pages/inicio.jsx
+++ src/pages/inicio.jsx
@@ -1,14 +1,18 @@
 import { useState } from "react";
-import logofrei from '../assets/logofrei.jpg';
+import logofrei from '../assets/logofrei.png';
 import { Link } from "react-router";
 import './inicio.scss'
+import '../styles/global.scss'
 
 export default function Inicio() {
     return(
         <div className="container-inicio">
-           <nav> <img src={logofrei} alt="logofrei" />
-            <Link to={"/"}>Inicio</Link>
-            <Link to={"/"}>Sobre</Link>
+           <nav> <img className="logoland" src={logofrei} alt="logofrei" />
+           <p className="text-logo">INSTITUTO SOCIAL NOSSA SENHORA DE <br /> FÁTIMA</p>
+            <div className="Batoes">
+            <Link to={"/"} className="Batao">Login Admin</Link>
+            <Link to={"/"} className="Batao">Conheça o Site Do Frei</Link>
+            </div>
             </nav>
         </div>
     )
